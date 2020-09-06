9c996c1bdd4a16b520ea89dbdeee70ab350b18f2
Loldozen <usmanloldozen@gmail.com>
2020-09-06 18:51:14 +0100

    Added automated tests

--- learn/API/tests.py
+++ learn/API/tests.py
@@ -19,11 +19,55 @@ class UserTest(APITestCase):
             'password':'random123',
         }
         response = self.client.post(self.create_url, data, format=None)
-        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objecself.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])ts.count(), 2)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['username'], data['username'])
-        self.assertEqual(response.data['email'], data['email'])
+        
         self.assertFalse('password' in response.data )
     
-    def test_create_children_profile(self):
-        pass
+    def test_create_user_with_no_email(self):
+        data={
+            'username':'example',
+            'email':'',
+            'password':'random123',
+        }
+        response = self.client.post(self.create_url, data, format=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(),1)
+        self.assertEqual(len(response.data['email']),1)
+
+    def test_create_user_with_invalid_email(self):
+        data={
+            'username':'example',
+            'email':'something',
+            'password':'random123',
+        }
+        response = self.client.post(self.create_url, data, format=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(),1)
+        self.assertEqual(len(response.data['email']), 1)
+
+    def test_create_user_with_existing_email(self):
+        data={
+            'username':'example',
+            'email':'test@gmail.com',
+            'password':'random123',
+        }
+        response = self.client.post(self.create_url, data, formay=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(),1)
+        self.assertEqual(len(response.data['email']), 1)
+
+        def update_user(self):
+            data={
+            'username':'usertest',
+            'email':'test@gmail.com',
+            'password':'random123',
+        }
+        self.create_url = reverse('parentprofile-detail')
+        response = self.client.put(self.create_url,data, format=None )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertFalse('password' in response.data)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+        self.assertEqual(User.objects.count(), 1)
